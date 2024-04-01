e0b4f4f430580c37d4a6169ec55ca94dcf4678bd
awanocode <anderson.awano@gmail.com>
2024-04-01 14:57:25 -0300

    arquivos verificados e funcinando

--- cartola-a/src/Home.jsx
+++ cartola-a/src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import './Home.css'
+import './Home.modules.css'
 import Logo from './assets/cartola.png'
 
 function Home() {
